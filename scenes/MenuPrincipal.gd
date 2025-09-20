61b571e6e537c95da0a83ffea3f7f6d2e662372f
maju2107 <maju.mariajuliaaraujo@gmail.com>
2025-09-20 20:53:18 -0300

    mudando  tipo de transição de cenas
    ps: Orquestrador não funcionou

--- scenes/MenuPrincipal.gd
+++ scenes/MenuPrincipal.gd
@@ -1,1 +1,5 @@
 extends ColorRect
+
+
+func _on_login_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/Login.tscn")
--- scenes/Orquestrador.gd
+++ scenes/Orquestrador.gd
@@ -1,11 +1,1 @@
 extends Node
-
-@onready var menu_principal = $MenuPrincipal
-@onready var login = $Login
-
-func _on_botaoVoltar_mp_pressed() -> void:
-	menu_principal.visible = true
-
-
-func _on_login_pressed() -> void:
-	menu_principal.visible = false
