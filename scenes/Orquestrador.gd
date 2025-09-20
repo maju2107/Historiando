00f2491421b341b6af6a597d84a39becde016ce1
maju2107 <maju.mariajuliaaraujo@gmail.com>
2025-09-20 12:59:00 -0300

    renomeação

--- scenes/Orquestrador.gd
+++ scenes/Orquestrador.gd
@@ -3,10 +3,9 @@ extends Node
 @onready var menu_principal = $MenuPrincipal
 @onready var login = $Login
 
-
-func _on_button_pressed() -> void:
-	menu_principal.visible = false
-
-
-func _on_button_2_pressed() -> void:
+func _on_botaoVoltar_mp_pressed() -> void:
 	menu_principal.visible = true
+
+
+func _on_login_pressed() -> void:
+	menu_principal.visible = false
