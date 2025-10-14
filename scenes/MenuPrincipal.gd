89ec6748cc974b9303fe98bf50725177f8ba8c39
maju2107 <maju.mariajuliaaraujo@gmail.com>
2025-10-13 22:44:29 -0300

    add mais coisinhas

--- scenes/MenuPrincipal.gd
+++ scenes/MenuPrincipal.gd
@@ -1,4 +1,4 @@
 extends ColorRect
 
 func _on_login_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/Login.tscn")
+	get_tree().change_scene_to_file("res://scenes/TelaInicial.tscn")
--- scenes/TelaInicial.gd
+++ scenes/TelaInicial.gd
@@ -1,6 +1,11 @@
 extends ColorRect
 
 @export var menu_principal : PackedScene
+@export var mecanica1 : PackedScene
 
 func _on_BotaoVoltar_pressed() -> void:
 	get_tree().change_scene_to_packed(menu_principal)
+
+
+func _on_button_mec_2_pressed() -> void:
+	get_tree().change_scene_to_packed(mecanica1)
