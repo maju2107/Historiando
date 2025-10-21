432e73049181574004e96445e31dcde80788d267
juliaaamendes <juliaaa.mendes.p@gmail.com>
2025-10-21 18:39:21 -0300

    camera e animações

---
+++ scenes/mecanica3/player.tres.gd
@@ -0,0 +1,97 @@
+extends CharacterBody3D
+
+
+const SPEED = 15.0
+const JUMP_VELOCITY = 6.0
+const ACCELERATION = 2.0
+
+@onready var camera_pivo: Node3D = $camera_pivo
+@onready var camera: Camera3D = $camera_pivo/camera
+@onready var gobot: Node3D = $gobot
+@onready var anim_player: AnimationPlayer = gobot.get_node("AnimationPlayer")
+
+var mouse_sensitivity: float = 0.15
+var camera_rotation: Vector2 = Vector2.ZERO
+var last_moviment_dir := Vector3.BACK
+var is_jumping
+
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	anim_player.play("Idle")
+
+func _unhandled_input(event: InputEvent) -> void:
+	var is_camera_motion := (
+		event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED
+	)
+	if is_camera_motion:
+		camera_rotation = event.screen_relative * mouse_sensitivity
+
+func  _input(_event: InputEvent) -> void:
+	if Input.is_action_just_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		
+	if Input.is_action_just_pressed("left_click"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _physics_process(delta: float) -> void:
+	# rotação da camera
+	camera_pivo.rotation.x += camera_rotation.y * delta
+	camera_pivo.rotation.x = clamp(camera_pivo.rotation.x, deg_to_rad(-75), deg_to_rad(20))
+	camera_pivo.rotation.y -= camera_rotation.x * delta
+	
+	camera_rotation = Vector2.ZERO
+	
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+		
+	is_jumping = Input.is_action_just_pressed("ui_accept") and is_on_floor()
+
+	# Handle jump.
+	if is_jumping:
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var forward := camera.global_basis.z
+	var right := camera.global_basis.x
+	var direction := (forward * input_dir.y + right * input_dir.x).normalized()
+	direction.y = 0.0
+	
+	var y_velocity = velocity.y
+	velocity.y = 0.0
+	
+	if is_on_floor():
+		if direction:
+			velocity = velocity.move_toward(direction * SPEED, ACCELERATION * delta)
+		else:
+			velocity = velocity.move_toward(Vector3.ZERO, ACCELERATION * delta)
+	
+	velocity.y = y_velocity
+	
+	_handle_animation()
+	move_and_slide()
+	
+	
+	if direction.length() >0.1:
+		last_moviment_dir = direction
+	
+	var target_angle := Vector3.BACK.signed_angle_to(last_moviment_dir, Vector3.UP)
+	gobot.global_rotation.y = lerp_angle(gobot.global_rotation.y, target_angle, ACCELERATION * delta)
+
+func _handle_animation():
+	if not is_on_floor():
+		if velocity.y > 0.1:
+			if anim_player.current_animation != "Jump":
+				anim_player.play("Jump", 0.2)
+		elif velocity.y < -0.1:
+			if anim_player.current_animation != "Fall":
+				anim_player.play("Fall", 0.2)
+	else:
+		if velocity.length() > 0.1:
+			if anim_player.current_animation != "Run":
+				anim_player.play("Run", 0.2)
+		else:
+			if anim_player.current_animation != "Idle":
+				anim_player.play("Idle", 0.2)
