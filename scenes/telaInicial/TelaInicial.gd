274532b93160caab9e0bd23e7d3d66f19d004f24
juliaaamendes <juliaaa.mendes.p@gmail.com>
2025-10-21 18:53:15 -0300

    botao menu para mecanica 3

--- scenes/telaInicial/TelaInicial.gd
+++ scenes/telaInicial/TelaInicial.gd
@@ -2,6 +2,7 @@ extends ColorRect
 
 @export var menu_principal : PackedScene
 @export var mecanica1 : PackedScene
+var mecanica3 = preload("res://scenes/mecanica3/mecanica_3.tscn")
 
 func _on_BotaoVoltar_pressed() -> void:
 	get_tree().change_scene_to_packed(menu_principal)
@@ -9,3 +10,6 @@ func _on_BotaoVoltar_pressed() -> void:
 
 func _on_button_mec_2_pressed() -> void:
 	get_tree().change_scene_to_packed(mecanica1)
+	
+func  _on_button_mec_3_pressed() -> void:
+	get_tree().change_scene_to_packed(mecanica3)
