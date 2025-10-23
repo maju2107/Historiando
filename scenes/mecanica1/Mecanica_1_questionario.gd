c04cc8f8e122988c3059229722761c3260f94da5
Maria Júlia <maju.mariajuliaaraujo@gmail.com>
2025-10-23 08:49:35 -0300

    add popups

--- scenes/mecanica1/Mecanica_1_questionario.gd
+++ scenes/mecanica1/Mecanica_1_questionario.gd
@@ -2,3 +2,27 @@ extends Node2D
 
 func _ready():
 	pass
+
+
+func _on_button_3_pressed() -> void:
+	$Popup1.popup_centered()
+	await get_tree().create_timer(2.0).timeout
+	$Popup1.hide() 
+
+
+func _on_button1_pressed() -> void:
+	$Popup2.popup_centered()
+	await get_tree().create_timer(2.0).timeout
+	$Popup2.hide()
+	
+
+func _on_button_4_pressed() -> void:
+	$Popup2.popup_centered()
+	await get_tree().create_timer(2.0).timeout
+	$Popup2.hide()
+	
+
+func _on_button_2_pressed() -> void:
+	$Popup2.popup_centered()
+	await get_tree().create_timer(2.0).timeout
+	$Popup2.hide()
