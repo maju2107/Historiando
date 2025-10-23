610d0d290a802f964de527b1670bc90e43dbb359
Maria Júlia <maju.mariajuliaaraujo@gmail.com>
2025-10-23 06:37:01 -0300

    texto label

--- scenes/mecanica1/Mecanica_1_questionario.gd
+++ scenes/mecanica1/Mecanica_1_questionario.gd
@@ -1,9 +1,10 @@
 extends Node2D
 
 var grupo: ButtonGroup
-var opcoes = ["Fogo", "Água", "Terra", "Ar"]
+var opcoes = ["A extinção da megafauna ocorreu devido a um grande meteoro que atingiu a Terra", "A extinção desses animais ocorreu devido ao aquecimento do clima e caça promovida pelos humanos ", "A extinção desses animais ocorreu devido a uma grande chuva toxica que os matou", "A extinção desses animais ocorreu devido a uma doença chamada Megafaunalite"]
 
 func _ready():
+    
     # Cria um CanvasLayer para desenhar UI sobre o mundo 2D
     var ui_layer = CanvasLayer.new()
     add_child(ui_layer)
@@ -26,3 +27,6 @@ func _ready():
         botao.button_group = grupo
         botao.connect("pressed", Callable(self, "_ao_pressionar").bind(botao))
         vbox.add_child(botao)
+    
+    var label = $ColorRect/Label
+    label.text = "O termo Megafauna é utilizado para se referir à diversidade de mamíferos gigantes que viveram no Pleistoceno (1,6 milhões de anos a 11.000 anos) e que atualmente encontram-se praticamente extintos. Sobre a extinção desses animais, escolha a alternativa correta:"
