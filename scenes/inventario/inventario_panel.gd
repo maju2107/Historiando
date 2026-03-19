8f180e8788ae87bb80f0bd618c2aa96e5290e2ec
Maria Júlia <maju.mariajuliaaraujo@gmail.com>
2026-03-19 12:31:25 -0300

    [create] Inicializando inventário
    Inicializando funcionalidade de inventário para o player

---
+++ scenes/inventario/inventario_panel.gd
@@ -0,0 +1,17 @@
+extends Panel
+
+
+func _process(delta: float) -> void: # tirar símbolo de bloqueio do mouse
+	if Input.get_current_cursor_shape() == CURSOR_FORBIDDEN:
+		DisplayServer.cursor_set_shape(DisplayServer.CURSOR_ARROW)
+		
+var dataFundo 
+
+func _notification(what: int) -> void: # impedir que o item suma na área não delimitada
+	if what == Node.NOTIFICATION_DRAG_BEGIN:
+		dataFundo = get_viewport().gui_get_drag_data()
+	if what == Node.NOTIFICATION_DRAG_END:
+		if not is_drag_successful():
+			if dataFundo:
+				dataFundo.icone.show()
+				dataFundo = null
---
+++ scenes/inventario/item_data.gd
@@ -0,0 +1,6 @@
+extends Resource
+class_name ItemData
+
+@export var item_nome: String
+@export var icone: Texture2D
+@export var mesh: Mesh
