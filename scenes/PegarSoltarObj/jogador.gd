f535e1ab687547d8ed4b23536b876e8645c97486
Maria Júlia <maju.mariajuliaaraujo@gmail.com>
2026-03-27 11:47:06 -0300

    [update] Adicionando sair com esc
    Sai do mundo ao clicar esc visto que o ponteiro do mouse está desabilitado

--- scenes/PegarSoltarObj/jogador.gd
+++ scenes/PegarSoltarObj/jogador.gd
@@ -4,8 +4,8 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
-# func _ready() :
-	#Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+func _ready() :
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
---
+++ scenes/PegarSoltarObj/mundo.gd
@@ -0,0 +1,5 @@
+extends Node
+
+func _unhandled_input(_event: InputEvent) -> void:
+	if Input.is_action_just_pressed("sair"):
+		get_tree().quit()
