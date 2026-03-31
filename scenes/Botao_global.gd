9dc687a3ac5c9ebfb77a073ef94771b1ee5d0858
Maria Júlia <maju.mariajuliaaraujo@gmail.com>
2026-03-31 09:36:40 -0300

    [update] Configurando câmera do jogador
    OBS: Câmera está lagada

--- scenes/Botao_global.gd
+++ scenes/Botao_global.gd
@@ -1,5 +1,8 @@
 extends CanvasLayer
 
-
 func BotaoGlobal_on_button_pressed() -> void:
 	get_tree().quit()
+	
+func _unhandled_input(_event: InputEvent) -> void:
+	if Input.is_action_just_pressed("sair"):
+		get_tree().quit()
--- scenes/PegarSoltarObj/jogador.gd
+++ scenes/PegarSoltarObj/jogador.gd
@@ -1,11 +1,21 @@
 extends CharacterBody3D
 
+@onready var cabeca: Node3D = $cabeca
+@onready var camera_3d: Camera3D = $cabeca/Camera3D
 
-const SPEED = 5.0
+const SPEED = 30.0
 const JUMP_VELOCITY = 4.5
+const MOUSE_SENSIBILITY: float = 0.0003
 
 func _ready() :
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED) # tirar o ponteiro do mouse da tela
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		cabeca.rotate_y(-event.relative.x * MOUSE_SENSIBILITY)
+		camera_3d.rotate_x(-event.relative.y * MOUSE_SENSIBILITY)
+		camera_3d.rotation.x = clamp(camera_3d.rotation.x, deg_to_rad(-90), deg_to_rad(90))
+
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -18,8 +28,9 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("left", "right", "up", "down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	var input_dir : Vector2 = Input.get_vector("left", "right", "up", "down")
+	var direction : Vector3 = (cabeca.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
