05157e86e63d7fac0ec7436d0b5069213e8b501a
Maria Júlia <maju.mariajuliaaraujo@gmail.com>
2026-04-09 12:05:22 -0300

    [update] Câmera rotacionando com inventário

--- scenes/PegarSoltarObj/jogadorPegar.gd
+++ scenes/PegarSoltarObj/jogadorPegar.gd
@@ -2,10 +2,9 @@ extends "res://scenes/parkour/player.gd"
 
 @onready var raycast : RayCast3D = $camera_pivo/camera/raycast
 @onready var hold_point: Marker3D = $camera_pivo/camera/hold_point
-@onready var canvas_layer: CanvasLayer = $"../mundo/CanvasLayer"
+@onready var canvas_layer: CanvasLayer = $"../CanvasLayer"
 
-
-func _unhandled_input(_event: InputEvent) -> void:
+func _input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("AbrirInventario"):
 		canvas_layer.show()
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
