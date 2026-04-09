20ea6fb7cec71b5ab22c4bdd06e904c08a7e4ff0
Maria Júlia <maju.mariajuliaaraujo@gmail.com>
2026-04-09 11:44:15 -0300

    [update] Aparecer inventário (não funcional)

--- scenes/PegarSoltarObj/jogadorPegar.gd
+++ scenes/PegarSoltarObj/jogadorPegar.gd
@@ -2,3 +2,11 @@ extends "res://scenes/parkour/player.gd"
 
 @onready var raycast : RayCast3D = $camera_pivo/camera/raycast
 @onready var hold_point: Marker3D = $camera_pivo/camera/hold_point
+@onready var canvas_layer: CanvasLayer = $"../mundo/CanvasLayer"
+
+
+func _unhandled_input(_event: InputEvent) -> void:
+	if Input.is_action_just_pressed("AbrirInventario"):
+		canvas_layer.show()
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		
