64ce31b126041cb5b506e0545fe22e3c8e08e879
Maria Júlia <maju.mariajuliaaraujo@gmail.com>
2026-04-14 11:59:32 -0300

    [update] Tentativa de inventário redondo

--- _testes/PegarSoltarObj/jogadorPegar.gd
+++ _testes/PegarSoltarObj/jogadorPegar.gd
@@ -1,4 +1,4 @@
-extends "res://scenes/parkour/player.gd"
+extends "res://_testes/parkour/player.gd"
 
 @onready var raycast : RayCast3D = $camera_pivo/camera/raycast
 @onready var hold_point: Marker3D = $camera_pivo/camera/hold_point
